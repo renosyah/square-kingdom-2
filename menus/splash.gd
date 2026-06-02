9d40ca306572f09050fa3fc41018e0393eb61931
renosyah <syahputrareno975@gmail.com>
2026-06-02 21:41:22 +0700

    add preset map

--- menus/splash.gd
+++ menus/splash.gd
@@ -3,5 +3,21 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	yield(get_tree().create_timer(0.8),"timeout")
+	yield(_unload_preset_map(),"completed")
 	Global.change_scene("res://menus/main_menu/main_menu.tscn", true)
+	
+func _unload_preset_map():
+	yield(get_tree(), "idle_frame")
+	var existing :PoolStringArray = Utils.get_all_resources("user://%s/" % Global.map_dir, ["manifest"])
+	if not existing.empty():
+		return
+		
+	yield(copy_preset_map(),"completed")
+	
+func copy_preset_map():
+	var maps :Array = Utils.get_all_resources("res://data/preset_map/", ["png","manifest","mission","map"])
+	for i in maps:
+		var file_path :String = i
+		Utils.copy_file_to_user(i, "user://%s/%s" % [Global.map_dir, file_path.get_file()])
+		yield(get_tree(), "idle_frame")
+		
